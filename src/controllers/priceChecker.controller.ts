820351e55b719452eeda8ddb1552c013f7bc93a5
Desarrollosyscomp <desarrollosyscomp@gmail.com>
2024-10-08 10:23:51 -0500

    cambios

--- src/controllers/priceChecker.controller.ts
+++ src/controllers/priceChecker.controller.ts
@@ -1,24 +1,20 @@
-import { Request, Response } from 'express'
-import { connect } from '../database';
+import { Request, Response } from "express";
+import { connect } from "../database";
 
-
-interface IsearchProduct{
-   barcode: string; 
+interface IsearchProduct {
+  barcode: string;
 }
 
-export class PriceChecker{
+export class PriceChecker {
+  /**
+   * Consultar productos por codigo de barras
+   */
 
-/**
- * Consultar productos por codigo de barras 
- */
-
-    static getProductByBarcode=async(req:Request, res:Response)=>{
-
-                try {
-                    
-                    const conn = await connect();
-                    const barcodeBody:IsearchProduct=req.body.barcode
-                   const searchProductBarcode= await conn.query(`SELECT
+  static getProductByBarcode = async (req: Request, res: Response) => {
+    try {
+      const conn = await connect();
+      const barcodeBody: IsearchProduct = req.body.barcode;
+      const searchProductBarcode = await conn.query(`SELECT
                     descripcion, precioventa, codigo, productos.valorico, productos.idproducto, barrasprod.barcode, productos.barcode
                 FROM
                    productos
@@ -27,17 +23,11 @@ export class PriceChecker{
                 WHERE
                      productos.barcode = ${barcodeBody} OR barrasprod.barcode = ${barcodeBody}
                 GROUP BY
-                    productos.idproducto`)   
-                    return res.status(200).json(searchProductBarcode[0]) 
-
-                    } catch (error) {
-                    
-                        console.log(error)
-                        return res.status(500).json({ error: error })                
-
-                    }
-
+                    productos.idproducto`);
+      return res.status(200).json(searchProductBarcode[0]);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: error });
     }
-
-
+  };
 }
